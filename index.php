0f48fd8c2cf3c9e23b0f250831b188f4b6c54182
K0Lb@zzeR <javasc@yandex.ru>
2023-09-28 09:02:10 +0700

    code refactoring

--- index.php
+++ index.php
@@ -10,6 +10,22 @@ class Player
         $this->name = $name;
         $this->coins = $coins;
     }
+
+    public function point(Player $player) : void
+    {
+        $this->coins++;
+        $player->coins--;
+    }
+
+    public function bankrupt() : bool
+    {
+        return $this->coins == 0;
+    }
+
+    public function bank() : int
+    {
+        return $this->coins;
+    }
 }
 
 class Game
@@ -24,25 +40,25 @@ class Game
         $this->player2 = $player2;
     }
 
+    public function flip() : string
+    {
+        // Подбросить монету
+        return rand(0, 1) ? "орел" : "решка"; 
+    }
+
     public function start()
     {
-        
         while(true){
-            // Подбросить монету
-            $flip = rand(0, 1) ? "орел" : "решка";
-
             // Если орел, п1 получает монету, п2 теряет
             // Если решка п1 теряет монету, п2 получает
-            if ($flip == "орел") {
-                $this->player1->coins++;
-                $this->player2->coins--;
+            if ($this->flip() == "орел") {
+                $this->player1->point($this->player2);
             } else {
-                $this->player1->coins--;
-                $this->player2->coins++;
+                $this->player2->point($this->player1);
             }
 
             // Если у кого-то кол-во монет будет 0, то игра окончена.
-            if ($this->player1->coins == 0 || $this->player2->coins == 0) {
+            if ($this->player1->bankrupt() || $this->player2->bankrupt()) {
                 return $this->end();
             }
 
@@ -51,14 +67,10 @@ class Game
         
     }
 
-    public function winner()
+    public function winner() : Player
     {
         //Победитель тот у кого больше монет.
-        if ($this->player1->coins > $this->player2->coins) {
-            return $this->player1;
-        } else {
-            return $this->player2;
-        }
+        return $this->player1->bank() > $this->player2->bank() ? $this->player1 : $this->player2;
         
     }
 
@@ -67,8 +79,8 @@ class Game
         echo <<<EOT
             Game over.
 
-            {$this->player1->name}: {$this->player1->coins}
-            {$this->player2->name}: {$this->player2->coins}
+            {$this->player1->name}: {$this->player1->bank()}
+            {$this->player2->name}: {$this->player2->bank()}
 
             Победитель: {$this->winner()->name}.
 
