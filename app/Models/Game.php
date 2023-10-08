6102db0392c7f1c7a55b16032074838327c3f6a5
K0Lb@zzeR <javasc@yandex.ru>
2023-10-08 18:48:11 +0700

    fix Game

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -10,94 +10,101 @@ class Game
 {
     /**
      * First player.
+     *
      * @var Player
      */
-    public Player $firstPlayer_;
+    private Player $firstPlayer_;
 
     /**
      * Second player.
+     *
      * @var Player
      */
-    public Player $secondPlayer_;
+    private Player $secondPlayer_;
 
     /**
      * Number of tosses.
+     *
      * @var int
      */
-    protected int $numberOfTosses_ = 1;
+    private int $numberOfTosses_ = 1;
 
     /**
      * __construct Game.
-     * @param Player $player1
-     * @param Player $player2
+     *
+     * @param Player $firstPlayer
+     * @param Player $secondPlayer
      */
-    public function __construct(Player $player1, Player $player2)
+    public function __construct(Player $firstPlayer, Player $secondPlayer)
     {
-        $this->firstPlayer_ = $player1;
-        $this->secondPlayer_ = $player2;
+        $this->setFirstPlayer($firstPlayer);
+        $this->setSecondPlayer($secondPlayer);
     }
 
     /**
-     * Подбрасываем монету.
+     * Toss a coin.
+     *
      * @return string
      */
-    public function flip(): string
+    public function tossCoin(): string
     {
-        return rand(0, 1) ? "орел" : "решка";
+        return rand(0, 1) ? "eagle" : "tails";
     }
 
     /**
      * Start Game.
+     *
      * @return void
      */
     public function startGame(): void
     {
         ViewGame::showPlayersChancesWinning(
-            $this->firstPlayer_->getPlayerName(),
-            $this->firstPlayer_->сalculatePlayersChancesOfWinning($this->secondPlayer_),
-            $this->secondPlayer_->getPlayerName(),
-            $this->secondPlayer_->сalculatePlayersChancesOfWinning($this->firstPlayer_)
+            $this->getFirstPlayer()->getPlayerName(),
+            $this->getFirstPlayer()->сalculatePlayersChancesOfWinning($this->getSecondPlayer()),
+            $this->getSecondPlayer()->getPlayerName(),
+            $this->getSecondPlayer()->сalculatePlayersChancesOfWinning($this->getFirstPlayer())
         );
 
-        $this->play();
+        $this->startingGameProcess();
     }
 
     /**
-     * Процесс игры.
+     * Starting the game process.
+     *
      * @return NULL
      */
-    public function play(): NULL
+    public function startingGameProcess(): NULL
     {
         while (true) {
-            // Если орел, п1 получает монету, п2 теряет
-            // Если решка п1 теряет монету, п2 получает
-            if ($this->flip() == "орел") {
-                $this->firstPlayer_->сhangeNumberOfCoinsPlayersHave($this->secondPlayer_);
+            if ($this->tossCoin() == "eagle") {
+                $this->getFirstPlayer()->сhangeNumberOfCoinsPlayersHave($this->getSecondPlayer());
             } else {
-                $this->secondPlayer_->сhangeNumberOfCoinsPlayersHave($this->firstPlayer_);
+                $this->getSecondPlayer()->сhangeNumberOfCoinsPlayersHave($this->getFirstPlayer());
             }
 
             // Если у кого-то кол-во монет будет 0, то игра окончена.
-            if ($this->firstPlayer_->isPlayerBankrupt() || $this->secondPlayer_->isPlayerBankrupt()) {
+            if ($this->getFirstPlayer()->isPlayerBankrupt() || $this->getSecondPlayer()->isPlayerBankrupt()) {
                 return $this->end();
             }
 
-            $this->numberOfTosses_++;
+            $this->setNumberOfTosses($this->getNumberOfTosses()+1);
         }
     }
 
     /**
      * Кто победитель?
      * Побеждает тот у кого больше монет.
+     *
      * @return Player
      */
     public function winner(): Player
     {
-        return $this->firstPlayer_->getNumberOfCoinsPlayerHas() > $this->secondPlayer_->getNumberOfCoinsPlayerHas() ? $this->firstPlayer_ : $this->secondPlayer_;
+        return $this->getFirstPlayer()->getNumberOfCoinsPlayerHas() > $this->getSecondPlayer()->getNumberOfCoinsPlayerHas() ? $this->getFirstPlayer() : $this->getSecondPlayer();
     }
 
     /**
      * Вывод результаты игры.
+     *
      * @return void
      */
     public function end(): void
@@ -105,12 +112,12 @@ class Game
         echo <<<EOT
             Game over.
 
-            {$this->firstPlayer_->getPlayerName()}: {$this->firstPlayer_->getNumberOfCoinsPlayerHas()} монет.
-            {$this->secondPlayer_->getPlayerName()}: {$this->secondPlayer_->getNumberOfCoinsPlayerHas()} монет.
+            {$this->getFirstPlayer()->getPlayerName()}: {$this->getFirstPlayer()->getNumberOfCoinsPlayerHas()} монет.
+            {$this->getSecondPlayer()->getPlayerName()}: {$this->getSecondPlayer()->getNumberOfCoinsPlayerHas()} монет.
 
             Победитель: {$this->winner()->getPlayerName()}.
 
-            Кол-во подбрасований: {$this->numberOfTosses_}.
+            Кол-во подбрасований: {$this->getNumberOfTosses()}.
         EOT;
     }
 
